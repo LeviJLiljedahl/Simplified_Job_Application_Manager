91676e98045072be32e43f70e8751044db49aa6a
lmarquardt7 <lmarquardt7@student.cvtc.edu>
2024-04-01 13:12:30 -0500

    initial import

---
+++ Simplified_JobApplicationManager/Simple_JAM_Form.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Simplified_JobApplicationManager
+{
+    partial class Simple_JAM_Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Simple_JAM_Form
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Simple_JAM_Form";
+            Text = "Job Application Manager - Simplified";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Simplified_JobApplicationManager/Simple_JAM_Form.cs
@@ -0,0 +1,10 @@
+namespace Simplified_JobApplicationManager
+{
+    public partial class Simple_JAM_Form : Form
+    {
+        public Simple_JAM_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
